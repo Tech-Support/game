07a1dd0692d3241034a4cd1fa7a8af286fa42f36
addisonbean <addisonbean@gmail.com>
2014-08-19 19:46:17 -0500

    added home screen

--- lib/windows.rb
+++ lib/windows.rb
@@ -3,17 +3,48 @@ class StartWindow < Gosu::Window
 		@width = width
 		@height = height
 		super(@width, @height, false)
-		@font = Gosu::Font.new(self, Gosu::default_font_name, 20)
+		@menu_items = %w[Start Help Options]
+		@selected_index = 0
 	end
 
 	def update
 	end
 
 	def draw
-		text = "Hello!"
-		x = @width / 2.0 - (@font.text_width(text) / 2.0)
-		y = @height / 3.0 - (@font.height / 2.0)
-		@font.draw(text, x, y, ZOrder::UI, 1, 1, 0xffffffff)
+		font = Gosu::Font.new(self, Gosu::default_font_name, 45)
+		title = "Hello!"
+		x = @width / 2.0 - (font.text_width(title) / 2.0)
+		y = @height * (5/12.0) - font.height
+		font.draw(title, x, y, ZOrder::UI, 1, 1, 0xffffffff)
+
+		font = Gosu::Font.new(self, Gosu::default_font_name, 20)
+		@menu_items.each_with_index do |text, i|
+			x = @width / 2.0 - (font.text_width(text) / 2.0)
+			y = @height * (7/12.0) - (font.height / 2.0)
+			y += (font.height + 10) * i
+			selected = text == @menu_items[@selected_index % @menu_items.size]
+			color = (selected ? 0xffffff00 : 0xffffffff)
+			font.draw(text, x, y, ZOrder::UI, 1, 1, color)
+		end
+	end
+
+	def button_down(id)
+		case id
+		when Gosu::KbDown
+			@selected_index += 1
+		when Gosu::KbUp
+			@selected_index -= 1
+		when 36 # Gosu::KbEnter should work, but it doesn't, I think it's 88 
+			selected = @menu_items[@selected_index % @menu_items.size]
+			case selected
+			when @menu_items[0]
+				close
+				game_win = GameWindow.new(@width, @height)
+				game_win.show
+			end
+		else
+			puts id
+		end
 	end
 end
 
